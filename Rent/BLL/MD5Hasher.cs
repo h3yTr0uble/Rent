8c2f9378afe88152ab4f4d96a557711ac4b40cd0
MaximRodkin <flash.l2@yandex.ru>
2019-04-19 01:02:41 +0300

    Реализовал класс для хеширование строк

---
+++ Rent/BLL/MD5Hasher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class MD5Hasher
+    {
+        public static string GetHash(string value)
+        {
+            MD5 hasher = MD5.Create();
+            byte[] hash = hasher.ComputeHash(Encoding.Unicode.GetBytes(value));
+
+            string hashed = "";
+            foreach (byte b in hash)
+            {
+                hashed += b.ToString("x2");
+            }
+
+            return hashed;
+        }
+    }
+}
